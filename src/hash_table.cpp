af040cfb99984d3c0168f927ee1e77ba30a25e3c
th0truth <shiftrams@gmail.com>
2026-03-28 19:57:09 +0200

    feat(resizing): add dynamic resizing hash table using Double hasing

--- src/hash_table.cpp
+++ src/hash_table.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 
 #include "hash_table.h"
+#include "prime.h"
 using namespace std;
 
 ht_item HashTable::DELETED_ITEM = {"", ""}; 
@@ -30,7 +31,7 @@ static int32_t ht_get_hash(string s, const int32_t num_buckets, const int32_t at
   return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
 
-ht_item *HashTable::new_item(string &key, string &value)
+ht_item *HashTable::new_item(const string &key, const string &value)
 {
   // Allocate an item
   ht_item *item = new ht_item;
@@ -47,14 +48,29 @@ void HashTable::del_item(ht_item *item)
 }
 
 // Insert a new key-value pair
-void HashTable::insert(string key, string value)
+void HashTable::insert(const string &key, const string &value)
 {
+  const int32_t load = this->count * 100 / this->size;
+  if (load > 70) {
+    resize_up();
+  }
+
   ht_item *item = new_item(key, value);
   int32_t index = ht_get_hash(item->key, this->size, 0);
   ht_item *current_item = this->items[index];
   int i = 1;
 
-  while (current_item != nullptr && current_item != &DELETED_ITEM) {
+  while (current_item != nullptr) {
+    if (current_item != &DELETED_ITEM) {
+      if (current_item->key == key) {
+        // KEY EXISTS: Delete the old one and replace it
+        del_item(current_item);
+        this->items[index] = item;
+        return; 
+      }
+    }
+
+    // Collision: move to the next index
     index = ht_get_hash(item->key, this->size, i);
     current_item = this->items[index];
     i++;
@@ -63,7 +79,7 @@ void HashTable::insert(string key, string value)
   this->count++;
 }
 
-string *HashTable::search(string key)
+string *HashTable::search(const string &key)
 {
   int32_t index = ht_get_hash(key, this->size, 0);
   ht_item *current_item = this->items[index];
@@ -81,15 +97,20 @@ string *HashTable::search(string key)
   return nullptr;
 }
 
-void HashTable::remove(string key)
+void HashTable::remove(const string &key)
 {
+  const int32_t load = this->count * 100 / this->size;
+  if (load < 10) {
+    resize_down();
+  }
+
   int32_t index = ht_get_hash(key, this->size, 0);
   ht_item *current_item = this->items[index];
   int i = 1;
 
   while (current_item != nullptr) {
     if (current_item != &DELETED_ITEM) {
-      if (!current_item->key.compare(key)) {
+      if (current_item->key == key) {
         del_item(current_item);
         this->items[index] = &DELETED_ITEM;
         this->count--;
@@ -104,7 +125,18 @@ void HashTable::remove(string key)
 
 HashTable::HashTable(void)
 {
-  this->size = 53;
+  this->base_size = HT_INITIAL_BASE_SIZE;
+  this->size = next_prime(this->base_size);
+  this->count = 0;
+
+  // Allocate an array of size containing pointers to ht_item.
+  this->items = new ht_item*[this->size]();
+}
+
+HashTable::HashTable(int32_t base_size)
+{
+  this->base_size = base_size;
+  this->size = next_prime(this->base_size);
   this->count = 0;
 
   // Allocate an array of size containing pointers to ht_item.
@@ -122,4 +154,47 @@ HashTable::~HashTable(void)
 
   // Delete the array of pointers
   delete[] this->items;
+}
+
+void HashTable::resize(const int32_t base_size)
+{
+  if (base_size < HT_INITIAL_BASE_SIZE) {
+    return;
+  }
+  
+  // Create a temporary new has table with the new size
+  HashTable new_ht(base_size);
+
+  // Re-hash all existing items into the new table
+  for (int i = 0; i < this->size; i++) {
+    ht_item *item = this->items[i];
+    if (item != nullptr && item != &DELETED_ITEM) {
+      new_ht.insert(item->key, item->value);
+    }  
+  }
+
+  // Update current table's attributes
+  this->base_size = new_ht.base_size;
+  this->count = new_ht.count;
+
+  // Swap the items arrays
+  const int32_t tmp_size = this->size;
+  this->size = new_ht.size;
+  new_ht.size = tmp_size;
+
+  ht_item **tmp_items = this->items;
+  this->items = new_ht.items;
+  new_ht.items = tmp_items;
+}
+
+void HashTable::resize_up()
+{
+  const int32_t new_size = this->base_size * 2;
+  resize(new_size);
+}
+
+void HashTable::resize_down()
+{
+  const int32_t new_size = this->base_size / 2;
+  resize(new_size);
 }
