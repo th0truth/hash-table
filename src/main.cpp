8e598d88acd942b5b5398e7134a9be19afe55768
th0truth <shiftrams@gmail.com>
2026-03-28 18:43:38 +0200

    feat: sample of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
 #include "hash_table.h"
+using namespace std;
 
 int main(void)
 {
   HashTable ht;
+
+  ht.insert("apple", "green");
+  string *result = ht.search("apple"); 
+  if (result != nullptr) {
+    cout << "Apple is " << *result << endl;
+  } else {
+    cout << "Apple not found" << endl;
+  }
+  return 0;
 }
